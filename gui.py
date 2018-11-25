39d548419dba069a29d7f4d4f10b8b7a2479f2f9
aakashsyadav <aakashyadav1911s@gmail.com>
2018-11-25 10:58:54 +0530

    dynamic ui

--- gui.py
+++ gui.py
@@ -34,6 +34,7 @@ window.geometry('700x450')
 
 # Radiobutton variable
 var = IntVar()
+var_d = IntVar()
 
 #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^from brain ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\
 pi = 3.14159265;
@@ -242,6 +243,177 @@ def create_window():
 
             # res = "Spring diameter " + str(xD) + "\nWire diameter "+str(xd)+"\nNa "+str(xNa)+"\nls "+str(xls)+"\nlo "+str(xlo)+"\nFigure of merit "+str(xfom)
             lbl_res.configure(text= res)
+
+        #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%   DESIGN.py DEFINITIONS
+        def calcMain_d():
+            # def calcMain(fmax, ymax, freelength, solidlength, material, endCondition):
+            # if inout in si
+            if var_d.get()==2:
+                fmax=float(txt_fmax.get())/4.44822
+                ymax=float(txt_ymax.get())/25.4
+                freelength=float(txt_freelength.get())/25.4
+                solidlength=float(txt_solidlength.get())/25.4
+            # if input in US
+            if var_d.get()==1:
+                fmax=float(txt_d_fmax.get())
+                ymax=float(txt_d_ymax.get())
+                freelength=float(txt_d_freelength.get())
+                solidlength=float(txt_d_solidlength.get())
+            #initial guess for 'd'
+            d=0.001
+            counter = 0
+            while d>0:
+                # factor of safety # TODO: take input
+                ns = 1.2;
+                # fixed pg510
+                zeta = 0.15
+                #for fom
+                gama = 1;
+                #for music wire
+                ## TODO: reduce size
+                material=combo_d_mat.get();
+                if material=='A228':
+                    if d <0.033:
+                        E=29.5*1000000
+                        G=12*1000000
+                    elif d >= 0.033 and d<=0.063:
+                        E=29*1000000
+                        G=11.85*1000000
+                    elif d > 0.063 and d<=0.125:
+                        E=28.5*1000000
+                        G=11.75*1000000
+                    elif d > 0.125:
+                        E=28*1000000
+                        G=11.6*1000000
+                    A=A228[1]*1000
+                    m=A228[0]
+                    # E=A228[2]*1000000
+                    # G=A228[3]*1000000
+                    rc=A228[5]
+                if material=='A229':
+                    if d <0.033:
+                        E=28.8*1000000
+                        G=11.7*1000000
+                    elif d >= 0.033 and d<=0.063:
+                        E=28.7*1000000
+                        G=11.6*1000000
+                    elif d > 0.063 and d<=0.125:
+                        E=28.6*1000000
+                        G=11.5*1000000
+                    elif d > 0.125:
+                        E=28.5*1000000
+                        G=11.4*1000000
+                    A=A229[1]*1000
+                    m=A229[0]
+                    # E=A229[2]*1000000
+                    # G=A229[3]*1000000
+                    rc=A229[5]
+                if material=='A227':
+                    A=A227[1]*1000
+                    m=A227[0]
+                    E=A227[2]*1000000
+                    G=A227[3]*1000000
+                    rc=A227[5]
+                if material=='A232':
+                    A=A232[1]*1000
+                    m=A232[0]
+                    E=A232[2]*1000000
+                    G=A232[3]*1000000
+                    rc=A232[5]
+                if material=='A401':
+                    A=A401[1]*1000
+                    m=A401[0]
+                    E=A401[2]*1000000
+                    G=A401[3]*1000000
+                    rc=A401[5]
+                # A = 201000 ;
+                # relative cost for mausc wire
+                # rc = 2.6;
+                # kpsi inch
+                # m = 0.145;
+                #E = 28.5;
+                # G = 11750000;
+                sut = A/(d**m);
+                ssy = 0.45*sut;
+                alpha = ssy/ns;
+                beta = (8*(1+zeta)*fmax)/(pi*(d**2));
+                C = (2*alpha-beta)/(4*beta) + (((2*alpha-beta)/(4*beta))**2 - (3*alpha)/(4*beta))**(0.5);
+                D = d*C;
+                # kb = (4*C+ 2)/(4*C - 3);
+                # taus = (kb*8*(1+zeta)*fmax*D)/(3.147*(d^3));
+                # OD = D+d;
+                Na = (G*(d**4)*ymax)/(8*(D**3)*fmax);
+                # checking end condition
+                combo_d_end['values']= ('Plain','Plain-Ground','Square-Closed','Square-Ground')
+
+                endCondition=combo_d_end.get()
+                if endCondition=='Plain':
+                    Nt=Na
+                    ls = d*(Nt+1)
+                if endCondition=='Plain-Ground':
+                    Nt=Na+1
+                    ls = d*Nt
+                if endCondition=='Square-Closed':
+                    Nt=Na+2
+                    ls = d*(Nt+1)
+                if endCondition=='Square-Ground':
+                    Nt=Na+2
+                    ls = d*Nt
+
+                lo = ls + (1+zeta)*ymax;
+                fom = -rc*gama*(pi**2)*(d**2)*Nt*D*0.25;
+
+                if isinstance(C, complex) or isinstance(Na, complex) or isinstance(ls, complex) or isinstance(lo, complex):
+                    print('complex values')
+                elif (C >= 4 and C <= 12 and Na >= 3 and Na <= 15 and ls < solidlength and lo < freelength):
+                    # break
+                    # return D, d, Na, ls, lo, fom
+                    if counter==0:
+                        if var_d.get()==2:
+                            xD=D*25.4
+                            xd=d*25.4
+                            xls=ls*25.4
+                            xlo=lo*25.4
+                        if var_d.get()==1:
+                            xD=D
+                            xd=d
+                            xls=ls
+                            xlo=lo
+                            # fmax=float(txt_fmax.get())
+                            # ymax=float(txt_ymax.get())
+                            # freelength=float(txt_freelength.get())
+                            # solidlength=float(txt_solidlength.get())
+                        # xD=D
+                        # xd=d
+                        xNa=Na
+                        # xls=ls
+                        # xlo=lo
+                        xfom=fom
+                    counter=counter+1
+                    f=open("res.txt", "a+")
+                    f.write("Wire diameter %f\r\n" % d)
+                    f.write("Spring diameter %f\r\n" % D)
+                    f.write("Na %f\r\n" % Na)
+                    f.write("ls %f\r\n" % ls)
+                    f.write("lo %f\r\n" % lo)
+                    f.write("Figure of merit %f\r\n" % fom)
+                    ts = time.time()
+                    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+                    f.write("____________"+st+"_____________\n")
+                    f.close()
+                elif d>1:
+                    print("iteration stopped")
+                    # messagebox.showinfo("Sorry! couldn't design the spring\nTIP:try different input")
+                    break
+                d = d+0.001;
+            # print(xD, xd, xNa, xls, xlo, xfom)
+            if var_d.get()==2:
+                res_d = "Spring diameter " + str(xD) + "mm\nWire diameter "+str(xd)+"mm\nNa "+str(xNa)+"\nls "+str(xls)+"mm\nlo "+str(xlo)+"mm\nFigure of merit "+str(xfom)
+            if var_d.get()==1:
+                res_d = "Spring diameter " + str(xD) + "inch\nWire diameter "+str(xd)+"inch\nNa "+str(xNa)+"\nls "+str(xls)+"inch\nlo "+str(xlo)+"inch\nFigure of merit "+str(xfom)
+
+            # res = "Spring diameter " + str(xD) + "\nWire diameter "+str(xd)+"\nNa "+str(xNa)+"\nls "+str(xls)+"\nlo "+str(xlo)+"\nFigure of merit "+str(xfom)
+            lbl_d_res.configure(text= res_d)
         #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^from brain ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
         def callback(event):
@@ -259,8 +431,19 @@ def create_window():
             lbl_ut_freelen.configure(text="inch")
             lbl_ut_solidlen.configure(text="inch")
 
+        def rad_d_si():
+            lbl_d_ut_fmx.configure(text="N")
+            lbl_d_ut_fmin.configure(text="N")
+            lbl_d_ut_ymx.configure(text="mm")
+            lbl_d_ut_freelen.configure(text="mm")
+            lbl_d_ut_solidlen.configure(text="mm")
 
-
+        def rad_d_us():
+            lbl_d_ut_fmx.configure(text="lbf")
+            lbl_d_ut_fmin.configure(text="lbf")
+            lbl_d_ut_ymx.configure(text="inch")
+            lbl_d_ut_freelen.configure(text="inch")
+            lbl_d_ut_solidlen.configure(text="inch")
         #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ radio units
         #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         # adding a menu
@@ -283,7 +466,6 @@ def create_window():
 
         note = ttk.Notebook(top)
         tab1 = ttk.Frame(note)
-
 
         headFrame = Frame(tab1)
         toolbar1 = Frame(tab1)
@@ -395,8 +577,135 @@ def create_window():
 
         note.add(tab1, text = "Spring design for Static load",compound=TOP)
         tab2 = ttk.Frame(note)
-        lbl = Label(tab2, text="gf")
-        lbl.pack(side = BOTTOM)
+
+        # tab2 starts 00000000000000000000000000000000000000000000000000000000000
+        headFrame_d = Frame(tab2)
+        toolbar1_d = Frame(tab2)
+        toolbar1_1_d = Frame(tab2)
+        toolbar2_d = Frame(tab2)
+        toolbar3_d = Frame(tab2)
+        toolbar4_d = Frame(tab2)
+        calcFrame_d = Frame(tab2)
+        toolbar5_d = Frame(tab2)
+        toolbar6_d = Frame(tab2)
+        toolbar7_d = Frame(tab2)
+        footFrame_d = Frame(tab2)
+
+        lbl = Label(headFrame_d, text="Input the given params",font=("Arial B",14))
+        lbl.pack(padx=2, pady=2)
+
+        headFrame_d.pack(side=TOP, fill=X, padx=4 , pady=20)
+
+        lbl_d = Label(toolbar1_d, text="Maximum force", width=15)
+        lbl_d.pack(side=LEFT, padx=2, pady=2)
+
+        txt_d_fmax = Entry(toolbar1_d,width=10)
+        txt_d_fmax.pack(side=LEFT, padx=2, pady=2)
+        txt_d_fmax.focus()
+
+        lbl_d_ut_fmx = Label(toolbar1_d, text="",font=("Arial B", 10), width=6)
+        lbl_d_ut_fmx.pack(side=LEFT, padx=2, pady=2)
+
+        lbl_d = Label(toolbar1_d, text="ymax", width=15)
+        lbl_d.pack(side=LEFT, padx=2, pady=2)
+
+        txt_d_ymax = Entry(toolbar1_d,width=10)
+        txt_d_ymax.pack(side=LEFT, padx=2, pady=2)
+
+        lbl_d_ut_ymx = Label(toolbar1_d, text="",font=("Arial B", 10), width=6)
+        lbl_d_ut_ymx.pack(side=LEFT, padx=2, pady=2)
+
+        toolbar1_d.pack(fill=X, padx=70 )
+
+        lbl_d = Label(toolbar1_1_d, text="Minimum force", width=15)
+        lbl_d.pack(side=LEFT, padx=2, pady=2)
+
+        txt_d_fmin = Entry(toolbar1_1_d,width=10)
+        txt_d_fmin.pack(side=LEFT, padx=2, pady=2)
+
+        lbl_d_ut_fmin = Label(toolbar1_1_d, text="",font=("Arial B", 10), width=6)
+        lbl_d_ut_fmin.pack(side=LEFT, padx=2, pady=2)
+
+        lbl_d = Label(toolbar1_1_d, text="Failiure Criteria", width=15)
+        lbl_d.pack(side=LEFT, padx=2, pady=2)
+
+        combo_d_fail = Combobox(toolbar1_1_d,width=10)
+        combo_d_fail['values']= ('Soderberg','Gerber','Gough','Yield')
+        combo_d_fail.current(1) #set the selected item
+        combo_d_fail.pack(side=LEFT, padx=2, pady=2)
+
+        toolbar1_1_d.pack(fill=X, padx=70 )
+
+        lbl_d = Label(toolbar2_d, text="Free Length", width=15)
+        lbl_d.pack(side=LEFT, padx=2, pady=2)
+
+        txt_d_freelength = Entry(toolbar2_d,width=10)
+        txt_d_freelength.pack(side=LEFT, padx=2, pady=2)
+
+        lbl_d_ut_freelen = Label(toolbar2_d, text="",font=("Arial B", 10), width=6)
+        lbl_d_ut_freelen.pack(side=LEFT, padx=2, pady=2)
+
+        lbl_d = Label(toolbar2_d, text="End condition", width=15)
+        lbl_d.pack(side=LEFT, padx=2, pady=2)
+
+        # txt = Entry(toolbar2,width=10)
+        # txt.pack(side=LEFT, padx=2, pady=2)
+        combo_d_end = Combobox(toolbar2_d,width=10)
+        combo_d_end['values']= ('Plain','Plain-Ground','Square-Closed','Square-Ground')
+        combo_d_end.current(1) #set the selected item
+        combo_d_end.pack(side=LEFT, padx=2, pady=2)
+
+        # lbl = Label(toolbar2, text="m/s^2",font=("Arial B", 10), width=6)
+        # lbl.pack(side=LEFT, padx=2, pady=2)
+
+        toolbar2_d.pack(fill=X, padx=70)
+
+        lbl_d = Label(toolbar3_d, text="Solid Length", width=15)
+        lbl_d.pack(side=LEFT, padx=2, pady=2)
+
+        txt_d_solidlength = Entry(toolbar3_d,width=10)
+        txt_d_solidlength.pack(side=LEFT, padx=2, pady=2)
+
+        lbl_d_ut_solidlen = Label(toolbar3_d, text="",font=("Arial B", 10), width=6)
+        lbl_d_ut_solidlen.pack(side=LEFT, padx=2, pady=2)
+
+        lbl_d = Label(toolbar3_d, text="Material", width=15)
+        lbl_d.pack(side=LEFT, padx=2, pady=2)
+
+        combo_d_mat = Combobox(toolbar3_d,width=5)
+        combo_d_mat['values']= ('A227','A228','A229','A232','A401')
+        combo_d_mat.current(1) #set the selected item
+        combo_d_mat.pack(side=LEFT, padx=2, pady=2)
+
+        toolbar3_d.pack(fill=X, padx=70)
+
+        lbl_d = Label(toolbar4_d, text="Output Units", width=15)
+        lbl_d.pack(side=LEFT, padx=2, pady=2)
+
+        rad1_d = Radiobutton(toolbar4_d,text='US', value=1, variable=var_d, command=rad_d_us)
+        rad1_d.pack(side=LEFT, padx=2, pady=2)
+
+        rad2_d = Radiobutton(toolbar4_d,text='SI', value=2, variable=var_d, command=rad_d_si)
+        rad2_d.pack(side=LEFT, padx=2, pady=2)
+
+        toolbar4_d.pack(fill=X, padx=70)
+
+        b_d = Button(calcFrame_d, text="Calculate",command=calcMain_d)
+        b_d.pack()
+        calcFrame_d.pack(fill=X, pady=20)
+
+        lbl_d = Label(toolbar5_d, text="Results", width=6,font=("Calibri",10))
+        lbl_d.pack(padx=2, pady=2)
+
+        link = Label(toolbar5_d, text="(more)", cursor="hand2")
+        link.pack()
+        link.bind("<Button-1>",callback)
+
+        lbl_d_res = Label(toolbar5_d, text="", width=40)
+        lbl_d_res.pack(padx=2, pady=2)
+
+        toolbar5_d.pack(side=BOTTOM)
+
         note.add(tab2, text = "Spring design for Dynamic load")
         note.pack()
 # function to create new window # TODO: Belleville SPRING  ******************************************
@@ -485,6 +794,7 @@ button1 = Button(topFrame, text="Helical Spring", command=create_window)
 button2 = Button(topFrame, text="Belleville Spring", command=create_window_2)
 button3 = Button(topFrame, text="Torsion Spring", command = create_window_2)
 button4 = Button(topFrame, text="Constant Force Spring", command = create_window_2)
+button5 = Button(topFrame, text="Leaf Spring", command = create_window_2)
 
 lbl.pack(side=TOP,pady=20)
 lbl2.pack()
@@ -492,6 +802,7 @@ button1.pack(fill=X,pady=5)
 button2.pack(fill=X,pady=5)
 button3.pack(fill=X,pady=5)
 button4.pack(fill=X,pady=5)
+button5.pack(fill=X,pady=5)
 
 # adding an image
 img = ImageTk.PhotoImage(Image.open("spr2_2.png"))
@@ -503,7 +814,7 @@ panel2.pack(side=LEFT)
 img3 = ImageTk.PhotoImage(Image.open("1.png"))
 panel3 = Label(imageFrame, image = img3)
 panel3.pack(side=LEFT)
-img4 = ImageTk.PhotoImage(Image.open("spr2_2.png"))
+img4 = ImageTk.PhotoImage(Image.open("Leaf_Spring_2.png"))
 panel4 = Label(imageFrame, image = img4)
 panel4.pack(side=LEFT)
 
@@ -517,8 +828,9 @@ new_item.add_command(label='Exit', command=window.quit)
 new_item2 = Menu(menu,tearoff=0)
 new_item2.add_command(label='Helical',command=create_window)
 new_item2.add_command(label='Belleville', command=create_window_2)
-new_item2.add_command(label='Torsion')
-new_item2.add_command(label='Constant Force')
+new_item2.add_command(label='Torsion', command=create_window_2)
+new_item2.add_command(label='Constant Force', command=create_window_2)
+new_item2.add_command(label='Leaf', command=create_window_2)
 menu.add_cascade(label='File',menu=new_item)
 menu.add_cascade(label='Start', menu=new_item2)
 menu.add_command(label='Save')
